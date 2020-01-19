34e159fd299e25ec04a574e39cd1eccc61a3854d
c4arl0s <c.santiago.cruz@gmail.com>
2020-01-18 22:32:05 -0600

    move files and add viewcontroller to record how to add a segue

--- ProgramaticSegues/CreateProgrammaticSegues/ViewController.swift
+++ ProgramaticSegues/CreateProgrammaticSegues/ViewController.swift
@@ -9,26 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var segueToggle: UISwitch!
+    @IBOutlet weak var segueSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     @IBAction func yellowButtonTapped(_ sender: Any) {
-        if segueToggle.isOn{
+        if segueSwitch.isOn {
             performSegue(withIdentifier: "Yellow", sender: nil)
         }
     }
     @IBAction func greenButtonTapped(_ sender: Any) {
-        if segueToggle.isOn{
+        if segueSwitch.isOn {
             performSegue(withIdentifier: "Green", sender: nil)
         }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
 
 
